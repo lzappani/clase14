12a116047f9fe1d6c5f41ff52c4f1be921cc6a52
lzappani <lzappani91@gmail.com>
2023-04-13 01:22:54 -0300

    Initial Commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class Main {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+// archivo archivo.txt, regex: [a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*
+// archivo shakespeare.txt, regex: (?<before>(?>\w+)[^\w]?\s(?>\w+)[^\w]?\s(?>\w+)[^\w]?)\sbreath[^\w]?\s(?<after>(?>\w+)[^\w]?\s(?>\w+)[^\w]?\s(?>\w+))
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> archivo = null;
+        String regex = null;
+        try {
+            archivo = FileReader(args[0]);
+            regex = args[1];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("ERROR! Argumentos incorrectos.");
+            System.exit(1);
+        } catch (IOException ioe) {
+            System.out.println("ERROR! Archivo no encontrado.");
+            System.exit(1);
+        }
+        Manipulador mani = new Manipulador(archivo, regex);
+
+        System.out.println("Exact matches by line = " + mani.countExactMatchesByLine());
+        System.out.println();
+        mani.printExactMatchesByLine();
+        System.out.println();
+        // mani.printExactMatchesByLine(true);
+        System.out.println();
+        System.out.println("Matches = " + mani.countMatches());
+        System.out.println();
+        mani.printMatches(true);
+    }
+
+    public static List<String> FileReader(String archivo) throws IOException {
+        return Files.readAllLines(Paths.get("src/main/resources/" + archivo));
+    }
 }
--- src/main/java/Manipulador.java
+++ src/main/java/Manipulador.java
@@ -1,2 +1,100 @@
-package PACKAGE_NAME;public class Manipulador {
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Manipulador {
+    private final List<String> archivo;
+    private final String regex;
+
+    public Manipulador(List<String> archivo, String regex) {
+        this.archivo = archivo;
+        this.regex = regex;
+    }
+
+    public int countExactMatchesByLine() {
+        int contador = 0;
+        for (String s : archivo) {
+            if (s.matches(regex)) {
+                contador++;
+            }
+        }
+        return contador;
+    }
+
+    public void printExactMatchesByLine() {
+        System.out.print("Computa si se corresponde con la expresión regular, linea por línea.\n");
+        if (this.countExactMatchesByLine() == 0) {
+            System.out.println("No hay matches");
+        } else {
+            for (int i = 0; i < archivo.size(); i++) {
+                if (archivo.get(i).matches(regex)) {
+                    System.out.printf("La línea número %d ", i + 1);
+                    System.out.print("se corresponde con la expresión regular.\n");
+                }
+            }
+        }
+    }
+
+    public void printExactMatchesByLine(boolean printNonMatches) {
+        System.out.print("Computa si se corresponde con la expresión regular, linea por línea.\n");
+        if (this.countExactMatchesByLine() == archivo.size()) {
+            System.out.println("Todas las lineas son matches");
+        } else {
+            for (int i = 0; i < archivo.size(); i++) {
+                if (archivo.get(i).matches(regex) && !printNonMatches) {
+                    System.out.printf("La línea número %d ", i + 1);
+                    System.out.print("se corresponde con la expresión regular.\n");
+                } else if (!archivo.get(i).matches(regex) && printNonMatches) {
+                    System.out.printf("La línea número %d ", i + 1);
+                    System.out.print("NO se corresponde con la expresión regular.\n");
+                }
+            }
+        }
+    }
+
+    public int countMatches() {
+        Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+        String texto = String.join("\n", archivo);
+        Matcher matcher = pattern.matcher(texto);
+        int contador = 0;
+        while (matcher.find()) {
+            contador++;
+        }
+        return contador;
+    }
+
+    public void printMatches() {
+        System.out.print("Colapsa el texto, e imprime los grupos por match.\n");
+        Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+        String texto = String.join("\n", archivo);
+        Matcher matcher = pattern.matcher(texto);
+
+        if (!matcher.find()) System.out.println("\tNo matches.");
+        matcher.reset();
+        int i = 1;
+        while (matcher.find()) {
+            System.out.printf("Match number %d: ", i++);
+            System.out.printf("%n\tFull match: %s%n", matcher.group(0));
+        }
+    }
+
+    public void printMatches(boolean printGroups) {
+        System.out.print("Colapsa el texto, e imprime los grupos por match.\n");
+        Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+        String texto = String.join("\n", archivo);
+        Matcher matcher = pattern.matcher(texto);
+
+        if (!matcher.find()) System.out.println("\tNo matches.");
+        matcher.reset();
+        int i = 1;
+        while (matcher.find()) {
+            System.out.printf("Match number %d: ", i++);
+            System.out.printf("%n\tFull match: %s%n", matcher.group(0));
+            if (printGroups) {
+                for (int j = 1; j <= matcher.groupCount(); j++) {
+                    System.out.printf("\t\tGroup %d : %s%n", j, matcher.group(j));
+                }
+            }
+        }
+    }
 }
